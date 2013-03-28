69284a009c1036b351f9d06bfc8ecf6ce5e8c660
georg wiltschek <georg.wiltschek@gmail.com>
2013-03-28 18:55:55 +0100

    magic 8-ball

--- quoteBot.rb
+++ quoteBot.rb
@@ -29,6 +29,14 @@ on :connect do
   join $config["config"]["default_channel"] 
 end
 
+# twitter search
+on :channel, /^!8 (.*?)$/ do |q|
+	# ignore q :p
+	answers = ["It is certain","It is decidedly so","Without a doubt","Yes – definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
+
+	msg channel, "#{answers.choice}."
+end
+
 # random wiki
 on :channel, /^!funfact$/ do
 	tries	  = 0
